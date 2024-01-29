b435979a5212d1c9efca0f129a82a3127f1dc575
NeverDecaf <jtr9289@gmail.com>
2024-01-29 01:33:26 -0500

    save blue banner status as well

--- shop_operators.py
+++ shop_operators.py
@@ -17,36 +17,38 @@ def get_operator_lists(live = True):
     else:
         with open('i_oplist_cache','r',encoding='utf8') as f:
             content = f.read()
-    tables = re.compile('<tbody>.*?<\/tbody>',re.DOTALL)
+    tables = re.compile('<tbody>.*?</tbody>',re.DOTALL)
     # skip the first table as it's just upcoming for global
     for table in tables.findall(content)[1:]:
-        rows = re.compile('<tr>.*?<\/tr>',re.DOTALL)
+        rows = re.compile('<tr>.*?</tr>',re.DOTALL)
         for row in rows.findall(table):
-            cn_date = re.compile('<td[^>]*views-field-field-cn-end-date[^>]*>(.*?)<\/td>',re.DOTALL)
-            na_date = re.compile('<td[^>]*views-field-field-start-time[^>]*>(.*?)<\/td>',re.DOTALL)
-            shop_ops = re.compile('<td[^>]*views-field-field-store-operators[^>]*>(.*?)<\/td>',re.DOTALL)
-            banner_ops = re.compile('<td[^>]*views-field-field-featured-characters[^>]*>(.*?)<\/td>',re.DOTALL)
-            time_parser = re.compile('<time[^>]*?>([^<]*)<\/time>',re.DOTALL)
-            op_parser = re.compile('<a[^>]*?>([^<]*)<\/a>',re.DOTALL)
+            cn_date = re.compile('<td[^>]*views-field-field-cn-end-date[^>]*>(.*?)</td>',re.DOTALL)
+            na_date = re.compile('<td[^>]*views-field-field-start-time[^>]*>(.*?)</td>',re.DOTALL)
+            shop_ops = re.compile('<td[^>]*views-field-field-store-operators[^>]*>(.*?)</td>',re.DOTALL)
+            banner_ops = re.compile('<td[^>]*views-field-field-featured-characters[^>]*>(.*?)</td>',re.DOTALL)
+            time_parser = re.compile('<time[^>]*?>([^<]*)</time>',re.DOTALL)
+            op_parser = re.compile('<a[^>]*?>([^<]*)</a>',re.DOTALL)
+            link_parser = re.compile('<td[^>]*views-field-field-event-banner[^>]*>[^<]*<a\s+href="([^"]*)',re.DOTALL)
             na_times = na_date.findall(row)[0]
             cn_times = cn_date.findall(row)[0]
             na_m = time_parser.match(na_times)
             cn_m = time_parser.match(cn_times)
+            sim_link = link_parser.findall(row)[0]
+            blue = int('#BB_' in sim_link)
             for op_name in op_parser.findall(shop_ops.findall(row)[0]):
-                
                 if na_m:
                     l = NA_OPS.setdefault(op_name,{'shop':[],'banner':[]})
-                    l['shop'].append(na_m.group(1))
+                    l['shop'].append({'date': na_m.group(1), 'blue': blue})
                 if cn_m:
                     l = CN_OPS.setdefault(op_name,{'shop':[],'banner':[]})
-                    l['shop'].append(cn_m.group(1))
+                    l['shop'].append({'date': cn_m.group(1), 'blue': blue})
             for op_name in op_parser.findall(banner_ops.findall(row)[0]):
                 if na_m:
                     l = NA_OPS.setdefault(op_name,{'shop':[],'banner':[]})
-                    l['banner'].append(na_m.group(1))
+                    l['banner'].append({'date': na_m.group(1), 'blue': blue})
                 if cn_m:
                     l = CN_OPS.setdefault(op_name,{'shop':[],'banner':[]})
-                    l['banner'].append(cn_m.group(1))
+                    l['banner'].append({'date': cn_m.group(1), 'blue': blue})
                     
 get_operator_lists(live=True)
 with open('./json/banner_history.json','w') as f:
